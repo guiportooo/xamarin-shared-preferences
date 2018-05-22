8092e19e60f0501afcc7b840d28873dfb7d1f216
Guilherme Porto <guiportooo@gmail.com>
2018-05-22 14:55:55 -0300

    Adding iOS implementation

---
+++ PreferencesPoc/PreferencesPoc.iOS/SharedPreferencesProvider.cs
@@ -0,0 +1,33 @@
+﻿
+using Foundation;
+using PreferencesPoc.iOS;
+
+[assembly: Xamarin.Forms.Dependency(typeof(SharedPreferencesProvider))]
+namespace PreferencesPoc.iOS
+{
+    public class SharedPreferencesProvider : ISharedPreferencesProvider
+    {
+        private NSUserDefaults userDefaults;
+
+        private NSUserDefaults GetUserDefaults(string nameSpace)
+            => userDefaults ?? new NSUserDefaults(nameSpace, NSUserDefaultsType.SuiteName);
+
+        public void Set(string nameSpace, string key, string value)
+        {
+            userDefaults = GetUserDefaults(nameSpace);
+            userDefaults.SetString(value, key);
+        }
+
+        public string Get(string nameSpace, string key, string defaultValue)
+        {
+            userDefaults = GetUserDefaults(nameSpace);
+            return userDefaults.StringForKey(key);
+        }
+
+        public void Remove(string nameSpace, string key)
+        {
+            userDefaults = GetUserDefaults(nameSpace);
+            userDefaults.RemoveObject(key);
+        }
+    }
+}
